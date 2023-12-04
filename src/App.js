ec1d96df7db7f881cff49df60cfdfba5a6930334
bita <shojabita@gmail.com>
2023-12-04 20:35:18 +0330

    change query

--- src/App.js
+++ src/App.js
@@ -6,28 +6,61 @@ import { useQuery, gql } from "@apollo/client";
 import { useEffect } from "react";
 import Profile from "./Components/profiles/Profile";
 
-const GET_DATA = gql`
+export const GET_CHARACTERS = gql`
   query characters($page: Int) {
     characters(page: $page) {
       info {
         count
       }
       results {
+        id
         name
         image
         status
-        species
-        type
-        gender
-        id
       }
     }
   }
 `;
+export const GET_CHARACTERS_BY_NAME = gql`
+  query characters($page: Int, $filter: FilterCharacter) {
+    characters(page: $page, filter: $filter) {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        image
+        status
+      }
+    }
+  }
+`;
+export const GET_CHARACTER_BY_IDS = gql`
+  query charactersByIds($ids: [ID!]!) {
+    charactersByIds(ids: $ids) {
+      id
+      name
+      image
+      status
+      gender
+      species
+      type
+      location {
+        name
+      }
+    }
+  }
+`;
+
 function App() {
-  const { loading, error, data } = useQuery(GET_DATA);
+  const { loading, error, data } = useQuery(
+    GET_CHARACTERS,
+    GET_CHARACTER_BY_IDS,
+    GET_CHARACTERS_BY_NAME,
+  );
   useEffect(() => {
-    console.log(data);
+    console.log(loading, error, data);
   });
   return (
     <ProfileContexts.Provider value={data}>
--- src/Components/profiles/Profile.jsx
+++ src/Components/profiles/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Profile.css";
 export default function Profile({ data }) {
