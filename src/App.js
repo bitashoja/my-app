fdef006fa69791fb723dca4d233ebbc0bc26b77d
Hamid Shoja <hamid.shoja@gmail.com>
2023-12-05 14:49:20 +0100

    move queries to graphql

--- src/App.js
+++ src/App.js
@@ -1,57 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Route, Routes } from "react-router-dom";
 import { ProfileContexts } from "./Components/contexts/ProfileContexts";
 import Profile from "./Components/profiles/Profile";
 import ProfileDetails from "./Components/profiles/ProfileDetails";
 import "./App.css";
 import * as React from "react";
-
-export const GET_CHARACTERS = gql`
-  query characters($page: Int, $name: String) {
-    characters(page: $page, filter: { name: $name }) {
-      info {
-        count
-      }
-      results {
-        id
-        name
-        image
-        status
-      }
-    }
-  }
-`;
-// export const GET_CHARACTERS_BY_NAME = gql`
-//   query characters($page: Int, $filter: FilterCharacter) {
-//     characters(page: $page, filter: $filter) {
-//       info {
-//         count
-//       }
-//       results {
-//         id
-//         name
-//         image
-//         status
-//       }
-//     }
-//   }
-// `;
-// export const GET_CHARACTER_BY_IDS = gql`
-//   query charactersByIds($ids: [ID!]!) {
-//     charactersByIds(ids: $ids) {
-//       id
-//       name
-//       image
-//       status
-//       gender
-//       species
-//       type
-//       location {
-//         name
-//       }
-//     }
-//   }
-// `;
+import { GET_CHARACTERS } from "./graphql/queries";
 
 function App() {
   const [page, setPage] = React.useState(1);
